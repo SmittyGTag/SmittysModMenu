e0c315e6603ee47fd64e134dc1c0e6e8d8d3b32b
SmittyGTag <157985396+SmittyGTag@users.noreply.github.com>
2024-03-02 12:10:24 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SmittyModMenu
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.Smitty.gorillatag.SmittyModMenu";
+        public const string Name = "Smitty's Mod Menu";
+        public const string Description = "Stupid monke";
+        public const string Version = "0.0.2";
+    }
+}
